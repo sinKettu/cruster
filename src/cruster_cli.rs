0fd9cf8a77ece7ceedccb57086dc7caa1c45573f
sinkettu <avangard.jazz@gmail.com>
2023-05-28 11:16:30 +0300

    Cruster 0.7.0 - cli: loading config

--- src/cruster_cli.rs
+++ src/cruster_cli.rs
@@ -1,5 +1,22 @@
-use newer_clap as clap;
+#![allow(dead_code)]
 
+mod config;
+
+use newer_clap as clap;
+use serde_yaml as yml;
+use shellexpand::tilde;
+use std::{fs, process::exit};
+
+#[derive(Debug)]
+struct CrusterCLIError {
+    error: String
+}
+
+impl<T> From<T> for CrusterCLIError where T: ToString {
+    fn from(e: T) -> Self {
+        Self { error: e.to_string() }
+    }
+}
 
 fn cli() -> clap::Command {
     clap::Command::new("cruster-cli")
@@ -37,16 +54,35 @@ fn cli() -> clap::Command {
         )
 }
 
-fn main() {
+fn main() -> Result<(), CrusterCLIError> {
     let command = cli().get_matches();
 
-    let config_path = command.get_one::<String>("CONFIG").unwrap();
-    let proj_path = command.get_one::<String>("PROJECT");
+    let config_path = tilde(command.get_one::<String>("CONFIG").unwrap());
+    let possible_proj_path = command.get_one::<String>("PROJECT");
+    let fin = fs::File::open(config_path.as_ref())?;
+    let config: config::Config = yml::from_reader(fin)?;
+
+    let project = if let Some(proj_path) = possible_proj_path {
+        proj_path.to_string()
+    }
+    else {
+        if let Some(proj_path) = config.project.as_ref() {
+            proj_path.to_string()
+        }
+        else {
+            eprintln!("Cannot find project to work with. Specify it neither with '-p' flag or in config");
+            exit(1);
+        }
+    };
 
     match command.subcommand() {
         Some(("http", subcommands)) => {
+            let http_data_path = format!("{}/http.jsonl", &project);
+            let http_data = fs::File::open(&http_data_path)?;
+
             match subcommands.subcommand() {
                 Some(("show", args)) => {
+                    // (?P<left_bound>\d+)(?P<range_or_strict>[-!])?(?P<right_bound>\d+)?
                     let range = args.get_one::<String>("INDEX").unwrap();
                     let urls = args.get_flag("urls");
 
@@ -59,4 +95,6 @@ fn main() {
             unreachable!()
         }
     }
+
+    Ok(())
 }
