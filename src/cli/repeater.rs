774435c78790516f26943a48fa3222bd58b1212a
sinkettu <avangard.jazz@gmail.com>
2023-05-30 00:31:03 +0300

    Cruster 0.7.0 - cli,repeater: iterator over repeaters implemented

--- src/cli/repeater.rs
+++ src/cli/repeater.rs
@@ -1,1 +1,39 @@
+use std::fs::File;
+use serde_json as json;
+use std::io::{BufReader, BufRead};
+use crate::siv_ui::repeater::{RepeaterState, RepeaterStateSerializable};
+
 pub(crate) mod list;
+pub(crate) mod show;
+
+
+pub(crate) struct RepeaterIterator {
+    reader: BufReader<File>
+}
+
+impl RepeaterIterator {
+    pub(crate) fn new(path: &str) -> RepeaterIterator {
+        let file = File::open(path).unwrap();
+        let iter = RepeaterIterator {
+            reader: BufReader::new(file)
+        };
+
+        return iter;
+    }
+}
+
+impl Iterator for RepeaterIterator {
+    type Item = RepeaterState;
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut buf = String::default();
+        let len = self.reader.read_line(&mut buf).unwrap();
+        if len == 0 {
+            return None
+        }
+
+        let ser_repeater = json::from_str::<RepeaterStateSerializable>(&buf).unwrap();
+        let repeater = RepeaterState::try_from(ser_repeater).unwrap();
+
+        return Some(repeater);
+    }
+}
--- src/cli/repeater/list.rs
+++ src/cli/repeater/list.rs
@@ -1,33 +1,25 @@
-use std::io::{BufReader, BufRead};
-
-use clap::ArgMatches;
-use serde_json as json;
-
 use crate::cli::CrusterCLIError;
 use crate::siv_ui::repeater::{
     RepeaterState,
-    RepeaterParameters,
-    RepeaterStateSerializable,
+    // RepeaterParameters,
+    // RepeaterStateSerializable,
 };
 
-pub(crate) struct RepeaterListSettings {
-    pub(crate) verbose: bool
-}
+use super::RepeaterIterator;
 
-impl TryFrom<&ArgMatches> for RepeaterListSettings {
-    type Error = CrusterCLIError;
-    fn try_from(args: &ArgMatches) -> Result<Self, Self::Error> {
-        let verbose = args.get_flag("verbose");
-        return Ok(
-            RepeaterListSettings {
-                verbose
-            }
-        );
+pub(super) fn print_repeater_state(repeater_state: &RepeaterState, number: usize) {
+    println!("\n{:<18}: {}", "Number", number + 1);
+    println!("{:<18}: {}", "Name", &repeater_state.name);
+    println!("{:<18}: {}", "Host", &repeater_state.parameters.address);
+    println!("{:<18}: {}", "HTTPS", &repeater_state.parameters.https);
+    println!("{:<18}: {}", "Redirects", &repeater_state.parameters.redirects);
+    if repeater_state.parameters.redirects {
+        println!("{:<18}: {}", "Maximum redirects", repeater_state.parameters.max_redirects)
     }
 }
 
 
-pub(crate) fn execute(settings: &RepeaterListSettings, repeater_state_path: &str) -> Result<(), CrusterCLIError> {
+pub(crate) fn execute(repeater_state_path: &str) -> Result<(), CrusterCLIError> {
     if ! std::path::Path::new(repeater_state_path).is_file() {
         return Err(
             CrusterCLIError::from(
@@ -36,22 +28,9 @@ pub(crate) fn execute(settings: &RepeaterListSettings, repeater_state_path: &str
         );
     }
 
-    let fin = std::fs::File::open(repeater_state_path)?;
-    let fin_reader = BufReader::new(fin);
-    for (i, line) in fin_reader.lines().enumerate() {
-        if let Ok(line) = line {
-            let ser_repeater: RepeaterStateSerializable = json::from_str(&line)?;
-            let repeater_state = RepeaterState::try_from(ser_repeater)?;
-
-            println!("\n{:<18}: {}", "Number", i + 1);
-            println!("{:<18}: {}", "Name", &repeater_state.name);
-            println!("{:<18}: {}", "Host", &repeater_state.parameters.address);
-            println!("{:<18}: {}", "HTTPS", &repeater_state.parameters.https);
-            println!("{:<18}: {}", "Redirects", &repeater_state.parameters.redirects);
-            if repeater_state.parameters.redirects {
-                println!("{:<18}: {}", "Maximum redirects", repeater_state.parameters.max_redirects)
-            }
-        }
+    let repeater_iter = RepeaterIterator::new(repeater_state_path);
+    for (i, repeater) in repeater_iter.enumerate() {
+        print_repeater_state(&repeater, i);
     }
 
     Ok(())
