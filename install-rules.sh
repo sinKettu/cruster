d577252f499f2a9b68a3a8989e42014f1204c300
sinkettu <avangard.jazz@gmail.com>
2023-07-28 10:25:06 +0300

    Cruster 0.8.0 - general: simple script to install rules

---
+++ install-rules.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+
+rules_dir="$(pwd)/rules"
+if ! [ -d "$rules_dir" ]; then
+    echo "'rules' directory is not found at current location; please, run the script from Cruster directory.";
+    exit 1;
+fi;
+
+cruster_base_dir="${HOME}/.cruster"
+if ! [ -d "$cruster_base_dir" ]; then
+    mkdir "$cruster_base_dir";
+fi;
+
+cp -r "$rules_dir" "${cruster_base_dir}/rules"
+echo "Rules are installed successfully into '${cruster_base_dir}/rules'"
