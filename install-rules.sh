2535d7d08d252663b416f844008b7827113869dd
sinKettu <avangard.jazz@gmail.com>
2024-07-27 23:15:22 +0000

    Cruster 0.8.0 - audit: minor improvements for rules installation script

--- install-rules.sh
+++ install-rules.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/sh
 
+if [ -f "$(pwd)/Cargo.toml" ]; then
+  project_name="$(bash -c 'cat Cargo.toml | head -2 | tail -1')"
+  if ! [ "$project_name" = "name = \"cruster\"" ]; then
+    echo "Script was luanched from the dir of project '$project_name'. But you must run this script from cruster project dir."
+    echo "Try: 'git clone https://github.com/sinKettu/cruster && cd cruster && bash ./install-rules.sh'"
+    exit 1
+  fi
+else
+    echo "Cannot find 'Cargo.toml' in current dir. You must run this script from cruster project dir."
+    echo "Try: 'git clone https://github.com/sinKettu/cruster && cd cruster && bash ./install-rules.sh'"
+    exit 1
+fi
+
 rules_dir="$(pwd)/rules"
 if ! [ -d "$rules_dir" ]; then
     echo "'rules' directory is not found at current location; please, run the script from Cruster directory.";
     exit 1;
 fi;
 
-cruster_base_dir="${HOME}/.cruster"
-if ! [ -d "$cruster_base_dir" ]; then
-    mkdir "$cruster_base_dir";
-fi;
-
+mkdir -p "~/.cruster"
 cp -v -r "$rules_dir" "${cruster_base_dir}/"
 
 if [ "$?" = "0" ]; then
